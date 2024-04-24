e0e6581c2d64b175c4571431abe95932896a4f75
Render78 <mariojuliantor@hotmail.com>
2024-04-23 21:06:44 -0300

    cart router added

--- src/app.js
+++ src/app.js
@@ -3,11 +3,13 @@ const path = require("path");
 const app = express();
 const PORT = 8080;
 const productsRouter = require("./routes/products.router.js");
+const cartsRouter = require("./routes/carts.router.js");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }))
 
 app.use("/", productsRouter);
+app.use("/", cartsRouter);
 
 app.listen(PORT, () => {
     console.log(`Server running on ${PORT}`);
