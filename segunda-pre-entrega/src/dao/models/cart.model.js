c8a09b9e6c115712dc2fb6925197d4bf13b96cdf
Render78 <mariojuliantor@hotmail.com>
2024-05-26 15:17:05 -0300

    cart model done with products array

---
+++ segunda-pre-entrega/src/dao/models/cart.model.js
@@ -0,0 +1,21 @@
+import mongoose from 'mongoose';
+
+const cartsCollection = "Carts";
+
+const cartsSchema = new mongoose.Schema({
+    products: {
+        type: [
+            {
+                product: {
+                    type: mongoose.Schema.Types.ObjectId,
+                    ref: "products"
+                }
+            }
+        ],
+        default: []
+    }
+});
+
+const cartsModel = mongoose.model(cartsCollection, cartsSchema);
+
+export default cartsModel;
