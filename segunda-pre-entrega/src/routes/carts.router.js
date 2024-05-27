6c38c94db90841d7118e735a95450bec1bd0aa11
Render78 <mariojuliantor@hotmail.com>
2024-05-27 20:27:48 -0300

    delete endpoint for delete one product working and update product's quantity endpoint working too

--- segunda-pre-entrega/src/routes/carts.router.js
+++ segunda-pre-entrega/src/routes/carts.router.js
@@ -66,5 +66,62 @@ router.post('/post/:cid/product/:pid', async (req, res) => {
     }
 });
 
+router.delete('/delete/:cid/products/:pid', async (req, res) => {
+    try {
+        const { cid, pid } = req.params;
+
+        const cart = await cartModel.findById(cid);
+        if (!cart) {
+            return res.status(404).json({ error: 'Carrito no encontrado' });
+        }
+
+        const existingProductIndex = cart.products.findIndex(item => item.product.toString() === pid);
+        if (existingProductIndex === -1) {
+            return res.status(404).json({ error: 'Producto no encontrado en el carrito' });
+        }
+
+        cart.products.splice(existingProductIndex, 1);
+
+        await cart.save();
+
+        return res.status(200).json(cart);
+    } catch (error) {
+        console.error('Error al eliminar el producto del carrito:', error);
+        return res.status(500).json({ error: 'Error al eliminar el producto del carrito' });
+    }
+});
+
+
+
+router.put('/put/:cid/products/:pid', async (req, res) => {
+    try {
+        const { cid, pid } = req.params;
+        const { quantity } = req.body;
+
+        if (typeof quantity !== 'number' || quantity <= 0) {
+            return res.status(400).json({ error: 'La cantidad debe ser un número positivo' });
+        }
+
+        const cart = await cartModel.findById(cid);
+        if (!cart) {
+            return res.status(404).json({ error: 'Carrito no encontrado' });
+        }
+
+        const existingProduct = cart.products.find(item => item.product.toString() === pid);
+        if (!existingProduct) {
+            return res.status(404).json({ error: 'Producto no encontrado en el carrito' });
+        }
+
+        existingProduct.quantity = quantity;
+
+        await cart.save();
+
+        return res.status(200).json(cart);
+    } catch (error) {
+        console.error('Error al actualizar la cantidad del producto en el carrito:', error);
+        return res.status(500).json({ error: 'Error al actualizar la cantidad del producto en el carrito' });
+    }
+});
+
 
 export default router;
