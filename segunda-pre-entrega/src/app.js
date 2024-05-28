c96a7556a99312e057336723747cc98d45bcfb87
Render78 <mariojuliantor@hotmail.com>
2024-05-28 10:28:26 -0300

    dotenv installed and working

--- segunda-pre-entrega/src/app.js
+++ segunda-pre-entrega/src/app.js
@@ -1,11 +1,11 @@
 import express from 'express';
 import mongoose from 'mongoose';
-import handlebars from 'express-handlebars'
+import handlebars from 'express-handlebars';
 import __dirname from './utils.js';
-import indexRouter from './routes/index.router.js'
-import productsRouter from './routes/products.router.js'
-import cartsRouter from './routes/carts.router.js'
-// import chatRouter from './routes/chat.router.js'
+import dotenv from 'dotenv'
+import indexRouter from './routes/index.router.js';
+import productsRouter from './routes/products.router.js';
+import cartsRouter from './routes/carts.router.js';
 
 const app = express();
 const PORT = 8080;
@@ -13,9 +13,13 @@ const PORT = 8080;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-mongoose.connect("mongodb+srv://Julian:12345@cluster0.e9to8uh.mongodb.net/ecommerce?retryWrites=true&w=majority&appName=Cluster0")
+dotenv.config();
+console.log(process.env);
+console.log(process.env.MONGO_URL);
+mongoose.connect(process.env.MONGO_URL)
     .then(() => { console.log("Conectado a la base de datos") })
-    .catch(error => { console.error("Error en la conexion", error) });
+    .catch(error => console.error("Error en la conexion", error))
+
 
 app.engine('handlebars', handlebars.engine())
 app.set('views', __dirname + '/views')
@@ -25,7 +29,6 @@ app.use(express.static(__dirname + '/public'))
 app.use('/', indexRouter);
 app.use('/api/products', productsRouter)
 app.use('/api/carts', cartsRouter)
-// app.use('/api/chat', chatRouter)
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
