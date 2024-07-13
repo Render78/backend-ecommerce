dc2385b6e4fe4fda76f90fc251df12911262b345
Render78 <mariojuliantor@hotmail.com>
2024-07-13 17:24:55 -0300

    product bussiness logic working, cart in development

---
+++ tercera-pre-entrega/src/repositories/cart.repository.js
@@ -0,0 +1,28 @@
+// src/repositories/cart.repository.js
+class CartRepository {
+    createCart() {
+        throw new Error("Method not implemented.");
+    }
+
+    getCartById(id) {
+        throw new Error("Method not implemented.");
+    }
+
+    addProductToCart(cartId, productId) {
+        throw new Error("Method not implemented.");
+    }
+
+    removeProductFromCart(cartId, productId) {
+        throw new Error("Method not implemented.");
+    }
+
+    updateProductQuantity(cartId, productId, quantity) {
+        throw new Error("Method not implemented.");
+    }
+
+    clearCart(cartId) {
+        throw new Error("Method not implemented.");
+    }
+}
+
+module.exports = CartRepository;
---
+++ tercera-pre-entrega/src/repositories/product.repository.js
@@ -0,0 +1,22 @@
+// src/repositories/product.repository.js
+export default class ProductRepository {
+    create(product) {
+        throw new Error("Method not implemented.");
+    }
+
+    findAll(filter, options) {
+        throw new Error("Method not implemented.");
+    }
+
+    findById(id) {
+        throw new Error("Method not implemented.");
+    }
+
+    update(id, product) {
+        throw new Error("Method not implemented.");
+    }
+
+    delete(id) {
+        throw new Error("Method not implemented.");
+    }
+}
